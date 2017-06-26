3cb63ffa3b9402020491a43828e8d91cba2122ba
Free Lancer <recnaleerf3@gmail.com>
2017-06-26 13:56:43 -0700

    Temp Rebrand to `Lyria`
    to allow for `AnCheera` and dev version to work alongside each other

--- src/pages/devtools/devtools.js
+++ src/pages/devtools/devtools.js
@@ -1,5 +1,5 @@
 (function() {
-  chrome.devtools.panels.create('AnCheera',
+  chrome.devtools.panels.create('Lyria',
     'assets/images/icon.png',
     'src/pages/devtools/default.html',
     function(panel) {
